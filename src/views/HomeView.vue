50cd4119de2ab0199abd5ac98dccd0eb67676898
julio cesar <jc10275@outlook.com>
2022-05-09 21:05:30 -0300

    Setup routes and create Home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,100 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="main">
+    <div class="wrapper">
+      <h1 style="font-weight: 300">
+        <span style="font-weight: 600">Github </span>
+        <i style="font-weight: 500; color: rgb(36, 39, 43)">Search</i>
+      </h1>
+      <form action="" method="get" @submit.prevent="onSubmit">
+        <div class="form-group form-group-row">
+          <div class="input-form">
+            <input type="text" name="" id="" v-model="inputData" />
+          </div>
+          <div class="input-form">
+            <button type="submit" class="btn-submit">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script setup>
+const { ref } = require("@vue/reactivity");
+const { useRouter } = require("vue-router");
+const inputData = ref("");
+const router = useRouter();
 
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
+function onSubmit() {
+  // Go to the search page
+  router.push({ name: "searchUser", params: { name: inputData.value } });
+}
 </script>
+
+<style scoped>
+h1 {
+  margin: 5px 0;
+}
+.main {
+  /* border: 1px solid red; */
+  width: 90%;
+  margin: 0 auto;
+  min-height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0px;
+}
+
+.main > .wrapper {
+  width: 35%;
+}
+
+.form-group {
+  width: 100%;
+}
+
+.form-group-row {
+  display: flex;
+  flex-direction: row;
+}
+
+.input-form {
+  width: 90%;
+}
+
+.input-form:nth-of-type(2) {
+  width: 17.5%;
+}
+
+.input-form > input[type="text"] {
+  width: 100%;
+  height: 33px;
+  outline: none;
+  border: 1px solid black;
+  padding: 7.5px;
+  font-size: 1rem;
+}
+.input-form > button {
+  height: 33px;
+  width: 100%;
+}
+
+.btn-submit {
+  border: none;
+  background-color: black;
+  cursor: pointer;
+  transition: 250ms ease;
+}
+
+.btn-submit > i {
+  color: white;
+}
+
+.btn-submit:hover {
+  background-color: rgb(33, 34, 35);
+}
+</style>
---
+++ src/views/SearchUser.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ user }}</h1>
+</template>
+
+<script setup>
+const { ref } = require("@vue/reactivity");
+const { useRoute } = require("vue-router");
+
+const route = useRoute();
+const user = ref("");
+user.value = route.params.name;
+
+console.log(route.params.name);
+</script>
+
+<style></style>
